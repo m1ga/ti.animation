c71951e132be427dfe4aec0dce9e505590df8a88
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-08 23:51:08 +0200

    Bump iOS version to 1.2.0

--- ios/Classes/TiAnimationModuleAssets.m
+++ ios/Classes/TiAnimationModuleAssets.m
@@ -7,14 +7,14 @@ extern NSData* filterDataInRange(NSData* thedata, NSRange range);
 
 @implementation TiAnimationModuleAssets
 
-- (NSData*) moduleAsset
+- (NSData *)moduleAsset
 {
 	
 
 	return nil;
 }
 
-- (NSData*) resolveModuleAsset:(NSString*)path
+- (NSData *)resolveModuleAsset:(NSString *)path
 {
 	
 
